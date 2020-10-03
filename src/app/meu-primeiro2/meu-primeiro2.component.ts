7afc5b1c87b10249e38fb2818098e0ee768d321d
Moisés Bueno <moibueno2011@hotmail.com>
2020-10-03 07:44:33 -0300

    Criando os componentes

---
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrls: ['./meu-primeiro2.component.css']
+})
+export class MeuPrimeiro2Component implements OnInit {
+
+  title: 'eu chamo teste';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
